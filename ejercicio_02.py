c2e90eee86c8a7039fe8ae4ed714858dfd48c65f
Allan Naranjo <allannaranjo66@gmail.com>
2026-09-13 20:25:43 -0600

    Ejercicios de variables y condicionales

---
+++ ejercicio_02.py
@@ -0,0 +1,20 @@
+"""number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")"""
+
+
+
+
+
+
+"""salary = float(input("Enter your salary: "))
+
+if salary < 1000:
+    print("You are not eligible for a bonus.")
+elif salary < 2000:
+    print("You are eligible for a small bonus.")
+else:
+    print("You are eligible for a large bonus.")"""
